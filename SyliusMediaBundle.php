ca99a4c12fa7380d5f506c8b2099375facaa1ecd
ツ Liverbool <nukboon@gmail.com>
2016-04-12 01:56:54 +0700

    bufgix.

--- SyliusMediaBundle.php
+++ SyliusMediaBundle.php
@@ -24,7 +24,7 @@ class SyliusMediaBundle extends AbstractResourceBundle
     /**
      * {@inheritdoc}
      */
-    public static function getSupportedDrivers()
+    public function getSupportedDrivers()
     {
         return array(
             SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
